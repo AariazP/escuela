c9babc281359f4ad8bf0a5a091e635051fee5910
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2024-05-26 17:24:28 -0500

    Cambiado el tipo de datos de la salide del procedimiento de Long a Character otra, otra vez

--- src/main/java/org/uniquindio/edu/co/escuela/controllers/AutenticationController.java
+++ src/main/java/org/uniquindio/edu/co/escuela/controllers/AutenticationController.java
@@ -21,9 +21,9 @@ public class AutenticationController {
 
         if (loginDTO.rol() == null || loginDTO.id() == null) {
             return ResponseEntity.badRequest().body(new MensajeDTO<>(true, "Credenciales invalidas", false));
-        } else if (autenticacionService.login(loginDTO) == 0) {
+        } else if (autenticacionService.login(loginDTO) == '0') {
             return ResponseEntity.badRequest().body(new MensajeDTO<>(true, "Usuario inexistente", false));
-        } else if (autenticacionService.login(loginDTO) == 1) {
+        } else if (autenticacionService.login(loginDTO) == '1') {
             return ResponseEntity.ok().body(new MensajeDTO<>(false, "Usuario correcto", true));
         } else {
             return ResponseEntity.badRequest().body(new MensajeDTO<>(true, "Todo mal", false));
